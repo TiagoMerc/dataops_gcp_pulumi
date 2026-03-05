e3694c9711de0bbd7a896549a40ca10774b62f7c
TiagoMerc <merc-s-tiago@hotmail.com>
2026-03-04 22:21:31 -0300

    Subindo nova fonte de dados csv, organizanodo o main, criando README fora do arquivo do projeto e desenhando a arquitetura

--- portfolio-dataops-gcp/__main__.py
+++ portfolio-dataops-gcp/__main__.py
@@ -34,13 +34,17 @@ analytics_dataset = bigquery.Dataset(
     location="US",
 )
 
+# Arquivo de vendas 
+# 4. Faz o upload do arquivo CSV local para dentro do Bucket
 arquivo_vendas = storage.BucketObject(
 "vendas-csv-upload",
 bucket=data_lake_bucket.name,
-source=pulumi.FileAsset("vendas.csv"),
-name="vendas.csv"
+source=pulumi.FileAsset("vendas.csv"),  # Lê o arquivo da nossa pasta
+name="vendas.csv" # Nome que o arquivo terá na nuvem
 )
 
+# 5. Cria a Tabela Externa no BigQuery
+# Detalhe de Portfólio: Tabelas externas não armazenam dados, apenas "leem" o Storage. Economiza custos!
 tabela_externa_vendas = bigquery.Table(
 "tabela_raw_vendas",
 dataset_id=raw_dataset.dataset_id,
@@ -48,11 +52,31 @@ table_id="vendas_externa",
 external_data_configuration=bigquery.TableExternalDataConfigurationArgs(
 source_uris=[pulumi.Output.concat("gs://", data_lake_bucket.name, "/", arquivo_vendas.name)],
 source_format="CSV",
-autodetect=True,
+autodetect=True, # O BigQuery descobre sozinho o que é número, texto ou data!
 )
 )
 
-# 4. Exportando os nomes reais que foram gerados na nuvem para o terminal
+# Adicionando o arquivo online_retail.csv ao Bucket
+arquivo_online_retail = storage.BucketObject(
+    "online-retail-csv-upload",
+    bucket=data_lake_bucket.name,
+    source=pulumi.FileAsset("online_retail.csv"),
+    name="online_retail.csv"
+)
+
+# Criando uma tabela externa para o arquivo online_retail.csv
+tabela_externa_online_retail = bigquery.Table(
+    "tabela_raw_online_retail",
+    dataset_id=raw_dataset.dataset_id,
+    table_id="online_retail_ecommerce",
+    external_data_configuration=bigquery.TableExternalDataConfigurationArgs(
+        source_uris=[pulumi.Output.concat("gs://", data_lake_bucket.name, "/", arquivo_online_retail.name)],
+        source_format="CSV",
+        autodetect=True,
+    )
+)
+
+# 6. Exportando os nomes reais que foram gerados na nuvem para o terminal
 pulumi.export("nome_do_bucket", data_lake_bucket.name)
 pulumi.export("dataset_raw", raw_dataset.dataset_id)
 pulumi.export("dataset_analytics", analytics_dataset.dataset_id)
